6a9302b862605064c637f1f2616115d73cb178f4
Administrator <294598473@qq.com>
2024-02-04 17:10:14 +0800

    223

--- read_data.py
+++ read_data.py
@@ -6,7 +6,7 @@ import yaml
 # print(data['heros_NameAndWord'])
 
 def get_data():
-    f = open("config/data.yml", 'r', encoding='utf-8')
+    f = open("F:\PycharmProjects\pytestcode\config\data.yml", 'r', encoding='utf-8')
     data = yaml.safe_load(f)
     return data['heros_NameAndWord']
 d=get_data()
